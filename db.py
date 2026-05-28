8e8b2b9a6f2700a93225822ea35ba6dc6f07dbb0
Alex Lescionok <alex.lescionok@gmail.com>
2026-05-28 07:30:04 +0100

    add new function to re-use connection between code and tests

--- db.py
+++ db.py
@@ -1,16 +1,19 @@
 import psycopg
+import os
+import dotenv
+from dotenv import load_dotenv
 
-def create_conversation():
-    # Connect to an existing database
-    with psycopg.connect(conninfo="postgresql://chatbot_user:localpassword@0.0.0.0:5432/chatbot") as conn:
+load_dotenv() 
 
-        # Open a cursor to perform database operations
-        with conn.cursor() as cur:
-            # Execute a command: this creates a new table
-            cur.execute("INSERT INTO conversations DEFAULT VALUES RETURNING id, session_id, created_at;")
-            row = cur.fetchone()
-            return {
-                "id": row[0],
-                "session_id": row[1],
-                "created_at": row[2]
-            }
+def get_conn():
+    return psycopg.connect(conninfo=os.environ["DATABASE_URI"])
+
+def create_conversation(conn):
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO conversations DEFAULT VALUES RETURNING id, session_id, created_at;")
+        row = cur.fetchone()
+        return {
+            "id": row[0],
+            "session_id": row[1],
+            "created_at": row[2]
+        }
--- test_db.py
+++ test_db.py
@@ -1,29 +1,32 @@
 
 import psycopg
-
 import db
+import pytest
 
-### DB tests
-def test_create_conversation_returns_id():
-    conversation = db.create_conversation()
+@pytest.fixture
+def db_conn():
+    with db.get_conn() as conn:
+        with conn.transaction():
+            yield conn
+            raise psycopg.Rollback()
+
+def test_create_conversation_returns_id(db_conn):
+    conversation = db.create_conversation(db_conn)
     assert conversation is not None
     assert isinstance(conversation, dict)
 
-def test_create_conversation_exists_in_db():
-    conversation_id = db.create_conversation()["id"]
+def test_create_conversation_exists_in_db(db_conn):
+    conversation_id = db.create_conversation(db_conn)["id"]
     
-    with psycopg.connect(conninfo="postgresql://chatbot_user:localpassword@0.0.0.0:5432/chatbot") as conn:
-
-        # Open a cursor to perform database operations
-        with conn.cursor() as cur:
-            cur.execute("SELECT id FROM conversations WHERE id = %s", (conversation_id,))
-            row = cur.fetchone()
+    with db_conn.cursor() as cur:
+        cur.execute("SELECT id FROM conversations WHERE id = %s", (conversation_id,))
+        row = cur.fetchone()
     
     assert row is not None
     assert row[0] == conversation_id
 
-def test_create_conversation_unique_ids():
-    conversation_id_1 = db.create_conversation()["id"]
-    conversation_id_2 = db.create_conversation()["id"]
+def test_create_conversation_unique_ids(db_conn):
+    conversation_id_1 = db.create_conversation(db_conn)["id"]
+    conversation_id_2 = db.create_conversation(db_conn)["id"]
 
     assert conversation_id_1 != conversation_id_2
